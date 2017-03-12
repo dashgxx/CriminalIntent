d83418409eef3f1535fa2eea8430cff94837d5ab
dashgxx <g382070527@163.com>
2017-03-12 21:49:26 +0800

    little modification: FragementPagerAdapter to FragmentStatePagerAdapter

--- app/src/main/java/com/example/xx/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/xx/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.net.ConnectException;
@@ -40,7 +41,7 @@ public class CrimePagerActivity extends FragmentActivity {
 
         mViewPager=(ViewPager)findViewById(R.id.activity_crime_pager_view_pagers);
         FragmentManager fm=getSupportFragmentManager();
-        mViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
             @Override
             public Fragment getItem(int position) {
                 Crime crime=mCrimes.get(position);
